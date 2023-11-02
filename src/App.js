d6c209bd7ae478b5be4497edef7afa7ecc06c337
amarpal2001 <amarpal19@navgurukul.org>
2023-11-02 08:26:41 +0530

    added lazy laoding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header'
 import Body from './components/Body'
@@ -7,9 +7,12 @@ import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Contact from './components/Contact'
 import Error from './components/Error'
 import RestaurantMenu from './components/RestaurantMenu'
+// import Grocery from './components/Grocery'
 
 
 
+
+const Grocery = lazy(() => import("./components/Grocery"))
 
 
 const AppLayout = () => {
@@ -38,6 +41,10 @@ const appRouter = createBrowserRouter([
       {
         path: '/contact',
         element: <Contact/>
+      },
+      {
+        path: '/grocery',
+        element: <Suspense fallback={<h1>Loading...</h1>}><Grocery/></Suspense>
       },
       {
         path: '/restaurant/:resId',
--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,8 @@ const Header = () => {
           <li> <Link to='/'>Home</Link></li>
           <li> <Link to='/About'>About Us</Link></li>
           <li> <Link to='/Contact'>Contact Us</Link></li>
-          <li>Card</li>
+          <li> <Link to='/Grocery'>Grocery</Link></li>
+          <li>Cart</li>
           <button
             className="login"
             onClick={() => {
